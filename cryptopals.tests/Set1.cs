ef7220e110cce4b4dac895f1dfda0aedc4446dec
Joshua Dow <Dow.c.joshua@gmail.com>
2018-05-05 16:30:09 -0400

    Solution to Set 1 Challenge 1

---
+++ cryptopals.tests/Set1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace cryptopals.tests
+{
+    [TestFixture]
+    public class Set1
+    {
+        [Test]
+        public void Challenge1()
+        {
+            var result = Crypto.HexStringToBase64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d");
+            Assert.AreEqual("SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t", result);
+        }
+    }
+}
---
+++ cryptopals/Crypto.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cryptopals
+{
+    public class Crypto
+    {
+        /// <summary>
+        /// Converts a hexadecimal string to its base64 representation
+        /// </summary>
+        /// <param name="hexString">The hexadecimal string to conver to base64</param>
+        /// <returns>The base64 representation of the hexadecimal string</returns>
+        public static string HexStringToBase64(string hexString)
+        {
+            // base64 encode our byte array and return it
+            return Convert.ToBase64String(HexStringToByteArray(hexString));
+        }
+
+        /// <summary>
+        /// Takes a hexadecimal string and returns its corresponding byte array
+        /// </summary>
+        /// <param name="hexString">A hexadecimal string</param>
+        /// <returns>The hexadecimal string as byte array</returns>
+        private static byte[] HexStringToByteArray(string hexString)
+        {
+            // divide by two since we're going from an array of hex digits (1 nibble)
+            // to an array of bytes (2 nibbles)
+            var byteArrayLength = hexString.Length / 2;
+            var bytes = new byte[byteArrayLength];
+            for (var i = 0; i < byteArrayLength; i++)
+            {
+                // split the contents into separate hex-number pairs
+                // then convert each into their byte value
+                bytes[i] = Convert.ToByte(hexString.Substring(i * 2, 2), 16);
+            }
+
+            return bytes;
+        }
+    }
+}
