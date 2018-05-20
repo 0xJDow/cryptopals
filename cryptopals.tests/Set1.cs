61ba86fbce44134bcbc92682f8b8b405f21d0264
Joshua Dow <Dow.c.joshua@gmail.com>
2018-05-20 01:23:39 -0400

    Solution to Set 1 Challenge 2

--- cryptopals.tests/Set1.cs
+++ cryptopals.tests/Set1.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System.Text;
+using NUnit.Framework;
 
 namespace cryptopals.tests
 {
@@ -10,6 +11,15 @@ namespace cryptopals.tests
         {
             var result = Crypto.HexStringToBase64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d");
             Assert.AreEqual("SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t", result);
+        }
+
+        [Test]
+        public void Challenge2()
+        {
+            var firstBuffer = Crypto.HexStringToByteArray("1c0111001f010100061a024b53535009181c");
+            var secondBuffer = Crypto.HexStringToByteArray("686974207468652062756c6c277320657965");
+            var result = Crypto.FixedXor(firstBuffer, secondBuffer);
+            Assert.AreEqual("746865206b696420646f6e277420706c6179", Crypto.ByteArrayToHexString(result));
         }
     }
 }
--- cryptopals/Crypto.cs
+++ cryptopals/Crypto.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net.NetworkInformation;
+using System.Threading;
 
 namespace cryptopals
 {
@@ -17,11 +19,35 @@ namespace cryptopals
         }
 
         /// <summary>
+        /// Takes two equal-length buffers and producers their xor combination
+        /// </summary>
+        /// <param name="firstBuffer">The first buffer</param>
+        /// <param name="secondBuffer">The second buffer</param>
+        /// <returns>The XOR combination of the two buffers</returns>
+        public static byte[] FixedXor(byte[] firstBuffer, byte[] secondBuffer)
+        {
+            // if the buffers aren't of equal length then that's a problem
+            if (firstBuffer.Length != secondBuffer.Length)
+                throw new ArgumentException("Buffers must be of equal length");
+
+            // instantiate a new byte array to hold the XOR output
+            byte[] output = new byte[firstBuffer.Length];
+
+            // iterate over the buffers and perform an XOR operation against each bit
+            for (var i = 0; i < firstBuffer.Length; i++)
+            {
+                output[i] = (byte)(firstBuffer[i] ^ secondBuffer[i]);
+            }
+
+            return output;
+        }
+
+        /// <summary>
         /// Converts a hexadecimal string to a byte array
         /// </summary>
         /// <param name="hexString">The hexadecimal string to convert</param>
         /// <returns>The hexadecimal string as a byte array</returns>
-        private static byte[] HexStringToByteArray(string hexString)
+        public static byte[] HexStringToByteArray(string hexString)
         {
             // the length of the byte array is going to be half the length of the hex string 
             // since a hex digit is equivalent to half a byte. (hex digit = 4 bits, byte = 8 bits)
@@ -35,6 +61,16 @@ namespace cryptopals
             }
 
             return bytes;
+        }
+
+        /// <summary>
+        /// Converts a byte array to a hexadecimal string
+        /// </summary>
+        /// <param name="byteArray">The byte array to convert</param>
+        /// <returns>The byte array as a hexadecimal string</returns>
+        public static string ByteArrayToHexString(byte[] byteArray)
+        {
+            return BitConverter.ToString(byteArray).Replace("-", "").ToLower();
         }
     }
 }
